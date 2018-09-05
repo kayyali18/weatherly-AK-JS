de5f677a7902881f749ab3cb5f933e3edc0f8a62
Ahmad Kayyali <kayyali18@gmail.com>
2018-09-04 19:36:02 -0600

    Succesfully pull data to make weather API request

--- weather-node/abb.js
+++ weather-node/abb.js
@@ -9,7 +9,9 @@ request ({
 }, (error, response, body) => { //parameters for callback are determined in the docs for request
     // callback runs when data is loaded in from google
     console.log (`Address: ${body.results[0].formatted_address} `);
-
+    console.log (`Latitude: ${body.results[0].geometry.location.lat} `);
+    console.log (`Longitude: ${body.results[0].geometry.location.lng} `);
+    
     // console.log (JSON.stringify(body, undefined, 2)); //second arg filters properties
     // console.log (JSON.stringify(response, undefined, 2)); //sends back status code
     // console.log (JSON.stringify(error, undefined, 2)); //status code 200 == A-ok
