e8667d4c60c3f285c59f9cc5961fffec07079b10
Ahmad Kayyali <kayyali18@gmail.com>
2018-09-04 20:02:07 -0600

    Format https request to be dynamic and encode URL

--- weather-node/abb.js
+++ weather-node/abb.js
@@ -1,9 +1,27 @@
 const request = require ('request');
+const yargs = require ('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true, //demand an address
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+
+let encodedAddress = encodeURIComponent(argv.address);
+console.log (encodedAddress)
 
 
 //first arg is options object to configure information (unique things to our request)
 request ({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20st%20philadelphia`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true, //tells it that whats coming back is JSON so it auto parses for us
 
 }, (error, response, body) => { //parameters for callback are determined in the docs for request
