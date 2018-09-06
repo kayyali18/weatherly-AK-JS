cc222fe2f3338338d901d8b01a75d2b99aec43ec
Ahmad Kayyali <kayyali18@gmail.com>
2018-09-05 20:33:54 -0600

    Modularise code

--- weather-node/abb.js
+++ weather-node/abb.js
@@ -1,5 +1,9 @@
-const request = require ('request');
 const yargs = require ('yargs');
+
+const request = require ('request')
+
+const geocode = require ('./geocode/geocode')
+const key = `a4aac8ef53f324af6467e2e607de880c`
 
 const argv = yargs
     .options({
@@ -14,27 +18,16 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-
-let encodedAddress = encodeURIComponent(argv.address);
-console.log (encodedAddress)
-
-
-//first arg is options object to configure information (unique things to our request)
-request ({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true, //tells it that whats coming back is JSON so it auto parses for us
-
-}, (error, response, body) => { //parameters for callback are determined in the docs for request
-    // callback runs when data is loaded in from google
-    console.log (`Address: ${body.results[0].formatted_address} `);
-    console.log (`Latitude: ${body.results[0].geometry.location.lat} `);
-    console.log (`Longitude: ${body.results[0].geometry.location.lng} `);
-    
-    // console.log (JSON.stringify(body, undefined, 2)); //second arg filters properties
-    // console.log (JSON.stringify(response, undefined, 2)); //sends back status code
-    // console.log (JSON.stringify(error, undefined, 2)); //status code 200 == A-ok
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) console.log (errorMessage);
+    console.log (JSON.stringify(results, undefined, 4));
+    request ({
+        url: `https://api.darksky.net/forecast/${key}/${results.latitude},${results.longitude}`,
+        json: true
+    }, (error, response, body) => {
+        console.log (body.currently)
+    })
 })
-
 
 
 
